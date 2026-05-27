fb132437e725c0766292b7c7a6d6e9f74555172c
sgurung183 <gurungsushant30@gmail.com>
2026-05-27 16:07:25 -0700

    initial springboot setup

---
+++ src/main/java/com/LittleDelhi/LittleDelhiBackend/LittleDelhiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.LittleDelhi.LittleDelhiBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleDelhiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleDelhiBackendApplication.class, args);
+	}
+
+}
