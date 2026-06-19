be59ba01cb7c8c7729e671e92d67f4a7f84e5e89
sgurung183 <gurungsushant30@gmail.com>
2026-06-19 16:41:34 -0700

    Latest Changes

--- src/main/java/com/LittleDelhi/LittleDelhiBackend/model/User.java
+++ src/main/java/com/LittleDelhi/LittleDelhiBackend/model/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 
 //tells springboot this is an entity class, reduces boiler plate code such as setters and getters
 @Entity
+@Table(name = "users")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
